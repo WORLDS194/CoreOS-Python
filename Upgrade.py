d3996be4296fdbae640c2c1bc70b9c20dcba0131
WORLDS <worldsbusiness15@gmail.com>
2026-06-03 16:48:34 -0400

    Add custom hello command to CoreOS update
    Override the CoreOS handle method to add a custom command.

--- Upgrade.py
+++ Upgrade.py
@@ -1,1 +1,16 @@
+print("[Upgrade] CoreOS updated successfully!")
+
+from System.CoreOS import CoreOS
+
+def hello(self, args):
+    print("Hello from the update!")
+
+old = CoreOS.handle
+
+def new_handle(self, cmd, args):
+    if cmd == "hello":
+        return hello(self, args)
+    return old(self, cmd, args)
+
+CoreOS.handle = new_handle
 
